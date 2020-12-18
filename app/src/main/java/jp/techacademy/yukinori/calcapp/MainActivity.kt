68c12435a7bed3da9789d1d16a8adc94f90a028e
shiota <mothership0018@gmail.com>
2020-12-18 23:29:47 +0900

    4th commit snackbarと第二画面遷移の条件分岐がうまくいかない。。。

--- app/src/main/java/jp/techacademy/yukinori/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yukinori/calcapp/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.View
 import android.content.Intent
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -16,30 +20,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button2.setOnClickListener(this)
         button3.setOnClickListener(this)
         button4.setOnClickListener(this)
+
+
     }
+
+
 
     override fun onClick(v: View?) {
         val intent = Intent(this, SecondClacApp::class.java)
         val edit_text_value1 = editText1.text.toString().toFloat()
         val edit_text_value2 = editText2.text.toString().toFloat()
 
-        if(v?.id == R.id.button1) {
-            val edit_text_value3 = edit_text_value1 + edit_text_value2
-            intent.putExtra("VALUE1", edit_text_value3)
-            startActivity(intent)
-        } else if(v?.id == R.id.button2){
+        if (v?.id == R.id.button1) {
+            if((editText1.text.isEmpty()) || (editText2.text.isEmpty())) {
+                    button1.setOnClickListener { view ->
+                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_INDEFINITE)
+                            .setAction("Action") {
+                                Log.d("UI_PARTS", "Snackbarをタップした")
+                            }.show()
+                    }
+            } else {
+                val edit_text_value3 = edit_text_value1 + edit_text_value2
+                intent.putExtra("VALUE1", edit_text_value3)
+                startActivity(intent)
+            }
+
+        } else if (v?.id == R.id.button2) {
             val edit_text_value3 = edit_text_value1 - edit_text_value2
             intent.putExtra("VALUE1", edit_text_value3)
             startActivity(intent)
-        } else if(v?.id == R.id.button3) {
+        } else if (v?.id == R.id.button3) {
             val edit_text_value3 = edit_text_value1 * edit_text_value2
             intent.putExtra("VALUE1", edit_text_value3)
             startActivity(intent)
-        } else if(v?.id == R.id.button4) {
+        } else if (v?.id == R.id.button4) {
             val edit_text_value3 = edit_text_value1 / edit_text_value2
             intent.putExtra("VALUE1", edit_text_value3)
             startActivity(intent)
-        } 
-
+        }
     }
 }
